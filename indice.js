cc6d70d377be2fda8ebb55fb8443cdbbeac9a49f
Flavia Martins <flaviamourapmartins@gmail.com>
2026-02-12 11:56:24 +0000

    update gameplay

--- indice.js
+++ indice.js
@@ -1,23 +1,31 @@
-console.log("Hello World");
 const ROCK = "ROCK", PAPER = "PAPER", SCISSORS = "SCISSORS";
 const game = [ROCK, PAPER, SCISSORS] // array
-const humanScore = 0, computerScore = 0, draw = 0, computerChoice = "", humanChoice = "", message = "What's your choice?", playRound = 5;
+let humanScore = 0, computerScore = 0, draw = 0, computerChoice = "", humanChoice = "", totalRounds = 5, currentRound = 1;
 
-function getComputerChoice() {
-  return Math.floor(Math.random() * game.length); // retorna valor aleatorio entre 0 e 2. random são números flutuantes e floor serve para arredondar os números.
+/**
+ * Decorrer do jogo
+ * 1. utilizador escolhe entre pedra, papel, tesoura
+ * 
+**/
+
+// elementos do jogo
+const buttons = document.getElementById('humanChoice').querySelectorAll("button"); // variável que esta armazenando os botoes
+const computerChoiceDiv = document.querySelector(".computerChoice")
+const points = document.querySelector(".points")
+
+function setHumanChoice(choice) {// função recebe a escolha do utilizador
+  humanChoice = choice;
 }
 
-function getHumanChoice(choice) {
-   const choice = prompt(message);
-  return choice.toUpperCase();
+function setComputerChoice() { // função escolha do computador é random
+  computerChoice = game[Math.floor(Math.random() * game.length)]; // retorna valor aleatorio entre 0 e 2. random são números flutuantes e floor serve para arredondar os números.
+  computerChoiceDiv.textContent = `Computer choice: ${computerChoice}`
 }
+ function addUpthedraws(){// funcao para somar os empates
 
-for( let i = 0; i<=playRound; i++)
-{
-    computerChoice = getComputerChoice(game)
-  /*console.log(computerChoice, game[computerChoice])*/
-    humanChoice = getHumanChoice()
-  /*console.log(humanChoice)*/
+ }
+ 
+function determineWinner() {  // função para deteminar quem ganhou a partida
 
   if (computerChoice === humanChoice) {
     console.log("It's a tie!")
@@ -37,15 +45,49 @@ for( let i = 0; i<=playRound; i++)
     console.log(`Point: ${computerScore}`);
     computerScore++;
   }
-  console.clear();
+  points.textContent = `Player: ${humanScore} - Computer: ${computerScore} - Draws: ${draw}`
 
 }
 
-if(humanScore > computerScore){
-  console.log("You win! Well played.");
-  console.log(`${humanScore} X ${computerScore}`);
+
+function renderResult() {
+  if (humanScore > computerScore) {
+    console.log("You win! Well played.");
+    console.log(`${humanScore} X ${computerScore}`);
+  }
+  else {
+    console.log("Game Over!You lost.")
+    console.log(`${computerScore} X ${humanScore}`);
+  }
 }
-else{
-  console.log("Game Over!You lost.")
-  console.log(`${computerScore} X ${humanScore}`);
+
+function isGameOver() {
+  return currentRound === totalRounds
 }
+
+function jogar(evento) {
+  const choice = evento.target.id;
+  setHumanChoice(choice)
+  setComputerChoice();
+  determineWinner();
+  renderResult();
+
+  if (isGameOver()) {
+    const botaoJogarNovamente = document.getElementById('jogar-novamente');
+    botaoJogarNovamente.hidden = false;
+    buttons.forEach((button) => {
+      button.removeEventListener("click", jogar)
+    });
+    botaoJogarNovamente.addEventListener('click', () => {
+      window.location.reload()
+    })
+    alert('FIM DE JOGO')
+    return;
+  }
+
+  currentRound++;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", jogar)
+});
