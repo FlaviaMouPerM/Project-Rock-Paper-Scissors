b6ff660bb20ea48adf04da5c233fe7107f8540f0
Flavia Martins <flaviamourapmartins@gmail.com>
2026-06-12 09:35:43 +0100

    Rename indice.html to index.html and indece.js to script.js

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ const ROCK = "ROCK", PAPER = "PAPER", SCISSORS = "SCISSORS";
 const game = [ROCK, PAPER, SCISSORS] // array
 let humanScore = 0, computerScore = 0, draw = 0, computerChoice = "", humanChoice = "", totalRounds = 5, currentRound = 1;
 
-/**
- * Decorrer do jogo
- * 1. utilizador escolhe entre pedra, papel, tesoura
- * 
-**/
+
 
 // elementos do jogo
 const buttons = document.getElementById('humanChoice').querySelectorAll("button"); // variável que esta armazenando os botoes
-const computerChoiceDiv = document.querySelector(".computerChoice")
+const computerChoiceDiv = document.querySelector(".computerChoice") // variavel que armazena a escolha do computados
 const points = document.querySelector(".points")
 
 function setHumanChoice(choice) {// função recebe a escolha do utilizador
@@ -21,9 +17,8 @@ function setComputerChoice() { // função escolha do computador é random
   computerChoice = game[Math.floor(Math.random() * game.length)]; // retorna valor aleatorio entre 0 e 2. random são números flutuantes e floor serve para arredondar os números.
   computerChoiceDiv.textContent = `Computer choice: ${computerChoice}`
 }
- function addUpthedraws(){// funcao para somar os empates
-
- }
+/* function addUpthedraws(){// funcao para somar os empates
+}*/
  
 function determineWinner() {  // função para deteminar quem ganhou a partida
 
